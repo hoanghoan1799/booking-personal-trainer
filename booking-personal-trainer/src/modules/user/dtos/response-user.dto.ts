b7fabcd41a7ff059e5d54b8132f43ea5a22de660
Hoan.Hoang <hoan.hoang@asnet.com.vn>
2026-02-03 15:06:50 +0700

    feat: define response user dto

---
+++ booking-personal-trainer/src/modules/user/dtos/response-user.dto.ts
@@ -0,0 +1,17 @@
+import {
+  TrainerApprovalStatus,
+  UserRole,
+  UserStatus,
+  UserType,
+} from '../../../common/enums/user/user.enum';
+
+export class ResponseUserDto {
+  userName: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  role: UserRole;
+  userType: UserType;
+  approvalStatus: TrainerApprovalStatus;
+  status: UserStatus;
+}
