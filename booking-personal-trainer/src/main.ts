6da7bdcdc93329a74d369235e58d599d699af203
Hoan.Hoang <hoan.hoang@asnet.com.vn>
2026-02-03 14:45:05 +0700

    feat: implement all exceptions filter and transform interceptor

--- booking-personal-trainer/src/main.ts
+++ booking-personal-trainer/src/main.ts
@@ -1,11 +1,44 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MikroORM } from '@mikro-orm/core';
+import { ValidationPipe } from '@nestjs/common';
 
 // Commons
 import { APP_PORT_DEFAULT } from './common/constants/app.constant';
+import { AllExceptionsFilter } from './common/filters/all-exception.filter';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
+  // Create app
   const app = await NestFactory.create(AppModule);
+
+  // Run migrations
+  const orm = app.get(MikroORM);
+  await orm.migrator.up();
+
+  // Global interceptors
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  // Global pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  // Global filters
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  // Start server
   await app.listen(process.env.PORT ?? APP_PORT_DEFAULT);
+
+  // Shutdown
+  app.enableShutdownHooks();
 }
+
 void bootstrap();
