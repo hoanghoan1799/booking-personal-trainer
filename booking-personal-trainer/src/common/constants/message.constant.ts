a0320c33fe132a696ca6d29b26f549a19fecb79f
Hoan.Hoang <hoan.hoang@asnet.com.vn>
2026-02-03 14:54:19 +0700

    feat: define register dto

---
+++ booking-personal-trainer/src/common/constants/message.constant.ts
@@ -0,0 +1,28 @@
+export const ERROR_MESSAGES = {
+  AUTH: {
+    INVALID_EMAIL: 'Email is invalid',
+    PASSWORD_MIN_LENGTH: (min: number) =>
+      `Password must be at least ${min} characters long`,
+    PASSWORD_TOO_LONG: 'Password must be at most 20 characters long',
+    PASSWORD_MISMATCH: 'Password does not match',
+    MISSING_REQUIRED_FIELDS: 'Required fields are missing',
+  },
+  USER: {
+    NOT_FOUND: 'User not found',
+    ALREADY_EXISTS: 'User already exists',
+    USERNAME_TAKEN: 'Username already exists',
+    EMAIL_TAKEN: 'Email already exists',
+  },
+  VALIDATION: {
+    USERNAME_REQUIRED: 'Username is required',
+    EMAIL_REQUIRED: 'User email is required',
+    PASSWORD_REQUIRED: 'User password is required',
+    LAST_NAME_REQUIRED: 'User last name is required',
+    FIRST_NAME_REQUIRED: 'User first name is required',
+    ROLE_REQUIRED: 'User role is required',
+    TYPE_REQUIRED: 'User type is required',
+  },
+  SYSTEM: {
+    INTERNAL_SERVER_ERROR: 'Internal server error',
+  },
+} as const;
---
+++ booking-personal-trainer/src/modules/auth/dtos/register.dto.ts
@@ -0,0 +1,61 @@
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+// Commons
+import {
+  TrainerApprovalStatus,
+  UserRole,
+  UserStatus,
+  UserType,
+} from '../../../common/enums/user/user.enum';
+import { VALIDATION } from '../../../common/constants/validation.constant';
+import { ERROR_MESSAGES } from 'src/common/constants/message.constant';
+
+export class RegisterDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(VALIDATION.PASSWORD_MIN_LENGTH, {
+    message: ERROR_MESSAGES.AUTH.PASSWORD_MIN_LENGTH(
+      VALIDATION.PASSWORD_MIN_LENGTH,
+    ),
+  })
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(VALIDATION.NAME_MAX_LENGTH)
+  userName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(VALIDATION.NAME_MAX_LENGTH)
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(VALIDATION.NAME_MAX_LENGTH)
+  lastName: string;
+
+  @IsEnum(UserType)
+  @IsNotEmpty()
+  userType: UserType;
+
+  @IsEnum(UserRole)
+  role: UserRole = UserRole.TRAINEE;
+
+  @IsEnum(TrainerApprovalStatus)
+  approvalStatus: TrainerApprovalStatus = TrainerApprovalStatus.NONE;
+
+  @IsEnum(UserStatus)
+  status: UserStatus = UserStatus.ACTIVE;
+}
