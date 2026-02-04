d85cd4b405fe409859daf42837161ad725ec20e3
Hoan.Hoang <hoan.hoang@asnet.com.vn>
2026-02-04 12:23:20 +0700

    feat: register redis module into app module

--- booking-personal-trainer/src/app.module.ts
+++ booking-personal-trainer/src/app.module.ts
@@ -1,6 +1,14 @@
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
+import { redisStore } from 'cache-manager-redis-store';
+
+// Constants
+import {
+  DEFAULT_HOST,
+  DEFAULT_REDIS_PORT,
+} from './common/constants/app.constant';
 
 // Modules
 import { DatabaseModule } from './modules/database/database.module';
@@ -8,17 +16,29 @@ import { UserModule } from './modules/user/user.module';
 import { BookingModule } from './modules/booking/booking.module';
 import { WorkoutModule } from './modules/workout/workout.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { RedisModule } from './modules/redis/redis.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        store: redisStore,
+        host: configService.get<string>('REDIS_HOST', DEFAULT_HOST),
+        port: configService.get<number>('REDIS_PORT', DEFAULT_REDIS_PORT),
+      }),
+      inject: [ConfigService],
+    }),
     DatabaseModule,
     UserModule,
     BookingModule,
     WorkoutModule,
     AuthModule,
+    RedisModule,
     PassportModule,
   ],
   controllers: [],
