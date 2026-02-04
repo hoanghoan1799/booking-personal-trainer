b5846e39fdb0d0243958f6074aa0f663556272f4
Hoan.Hoang <hoan.hoang@asnet.com.vn>
2026-02-04 12:15:15 +0700

    feat: define global constant

--- booking-personal-trainer/src/common/constants/app.constant.ts
+++ booking-personal-trainer/src/common/constants/app.constant.ts
@@ -1,2 +1,4 @@
 export const APP_PORT_DEFAULT = 3000 as const;
 export const POSTGRES_PORT_DEFAULT = 5432 as const;
+export const DEFAULT_REDIS_PORT = 6379 as const;
+export const DEFAULT_HOST = 'localhost' as const;
