6c1cd0a620f5acc2af219353fe91b8c01ba20a10
Hoan.Hoang <hoan.hoang@asnet.com.vn>
2026-02-05 12:07:52 +0700

    feat: implement update user role api

--- booking-personal-trainer/src/modules/user/user.controller.ts
+++ booking-personal-trainer/src/modules/user/user.controller.ts
@@ -1,12 +1,39 @@
 // TODO: Need to implement
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Req, UseGuards } from '@nestjs/common';
+
+// Commons
+import { UserRole } from '../../common/enums/user/user.enum';
+import { Roles } from '../../common/decorators/role.decorator';
+import { RolesGuard } from '../../common/guards/roles.guard';
+
+// Entities
+import { User } from './entities/user.entity';
 
 // DTOs
+import { UpdateUserRoleDto } from './dtos/update-user.dto';
 
 // Services
 import { UserService } from './user.service';
 
-@Controller('user')
+// Guards
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+
+export interface RequestWithUser extends Request {
+  user: User;
+}
+
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(UserRole.ADMIN)
+@Controller('admin/users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Patch(':userId/role')
+  async updateUserRole(
+    @Param('userId') id: string,
+    @Body() data: UpdateUserRoleDto,
+    @Req() req: RequestWithUser,
+  ) {
+    return this.userService.updateUserRole(id, data, req.user);
+  }
 }
