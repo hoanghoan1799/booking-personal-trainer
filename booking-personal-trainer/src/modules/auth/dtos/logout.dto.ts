fb845096488593afbcaffb1d7ca9b31ceddddea0
Hoan.Hoang <hoan.hoang@asnet.com.vn>
2026-04-13 12:09:31 +0700

    fix: update logout dto

--- booking-personal-trainer/src/modules/auth/dtos/logout.dto.ts
+++ booking-personal-trainer/src/modules/auth/dtos/logout.dto.ts
@@ -1,4 +1,11 @@
+import { IsOptional, IsString, IsUUID } from 'class-validator';
+
 export class LogoutDto {
+  @IsOptional()
+  @IsUUID()
   userId?: string;
+
+  @IsOptional()
+  @IsString()
   refreshToken?: string;
 }
